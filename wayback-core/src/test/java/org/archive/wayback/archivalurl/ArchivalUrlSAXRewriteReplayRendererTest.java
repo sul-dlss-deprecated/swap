2fae969b8d69eeea063182e3307d28d335b10ac8
Naomi Dushay <ndushay@stanford.edu>
2017-02-08 16:10:34 -0800

    core: ArchivalUrlSAXRewriteReplayRendererTest - include X-UA-Compatible header

--- wayback-core/src/test/java/org/archive/wayback/archivalurl/ArchivalUrlSAXRewriteReplayRendererTest.java
+++ wayback-core/src/test/java/org/archive/wayback/archivalurl/ArchivalUrlSAXRewriteReplayRendererTest.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.archive.wayback.archivalurl;
 
@@ -43,9 +43,9 @@ public class ArchivalUrlSAXRewriteReplayRendererTest extends TestCase {
     WaybackRequest wbRequest;
     CaptureSearchResult result;
     TestServletOutputStream servletOutput = new TestServletOutputStream();
-    
+
     ArchivalUrlSAXRewriteReplayRenderer cut;
-    
+
     public static class TestParseEventHandler implements ParseEventHandler {
         @Override
         public void handleParseStart(ParseContext context) {
@@ -62,7 +62,7 @@ public class ArchivalUrlSAXRewriteReplayRendererTest extends TestCase {
         public void handleParseComplete(ParseContext context) {
         }
     }
-    
+
     /* (non-Javadoc)
      * @see junit.framework.TestCase#setUp()
      */
@@ -70,21 +70,21 @@ public class ArchivalUrlSAXRewriteReplayRendererTest extends TestCase {
         super.setUp();
         RedirectRewritingHttpHeaderProcessor httpHeaderProcessor = new RedirectRewritingHttpHeaderProcessor();
         httpHeaderProcessor.setPrefix("X-Archive-Orig-");
-        
+
         cut = new ArchivalUrlSAXRewriteReplayRenderer(httpHeaderProcessor);
-        
+
         uriConverter = EasyMock.createMock(ResultURIConverter.class);
-        
+
         response = EasyMock.createMock(HttpServletResponse.class);
         EasyMock.expect(response.getOutputStream()).andReturn(servletOutput);
-        
+
         nodeHandler = EasyMock.createMock(ParseEventHandler.class);
         cut.setDelegator(nodeHandler);
-        
+
         wbRequest = new WaybackRequest();
         wbRequest.setFrameWrapperContext(false);
-        
-        // replace default CharsetDetector (StandardCharsetDetector) with a stub 
+
+        // replace default CharsetDetector (StandardCharsetDetector) with a stub
         // so as not to depend on its behavior.
         cut.setCharsetDetector(new CharsetDetector() {
             @Override
@@ -93,11 +93,11 @@ public class ArchivalUrlSAXRewriteReplayRendererTest extends TestCase {
                 return "UTF-8";
             }
         });
-        
+
         result = new CaptureSearchResult();
         result.setOriginalUrl("http://www.example.com/");
     }
-    
+
     public static Resource createTestHtmlResource(byte[] payloadBytes) throws IOException {
         WARCRecordInfo recinfo = TestWARCRecordInfo.createCompressedHttpResponse("text/html", payloadBytes);
         TestWARCReader ar = new TestWARCReader(recinfo);
@@ -115,14 +115,14 @@ public class ArchivalUrlSAXRewriteReplayRendererTest extends TestCase {
         resource.parseHeaders();
         return resource;
     }
-    
+
     /**
      * test basic behavior with simple input.
      * expectations:
      * <ul>
      * <li>reads <em>decoded (uncompressed)</em> contents from archive record.</li>
      * <li>calls delegator.handleParseStart() and handleParseComplete() just once, respectively.</li>
-     * <li>calls HttpServletResponse.setHeader() for Content-Type, Content-Length and 
+     * <li>calls HttpServletResponse.setHeader() for Content-Type, Content-Length and
      *   {@link TextReplayRenderer#GUESSED_CHARSET_HEADER} (not configurable as with TextReplayRenderer
      *   subclasses.</li>
      * <li>calls HttpServletResponse.setCharsetEncoding() with value "utf-8"</li>
@@ -136,7 +136,7 @@ public class ArchivalUrlSAXRewriteReplayRendererTest extends TestCase {
         String payload = "<HTML></HTML>\n";
         final byte[] payloadBytes = payload.getBytes("UTF-8");
         Resource payloadResource = createTestHtmlResource(payloadBytes);
-        
+
         Capture<ReplayParseContext> parseContextCapture = new Capture<ReplayParseContext>();
         Capture<Node> nodeCapture = new Capture<Node>();
         nodeHandler.handleParseStart(EasyMock.<ReplayParseContext>anyObject());
@@ -150,25 +150,26 @@ public class ArchivalUrlSAXRewriteReplayRendererTest extends TestCase {
         response.setHeader("Content-Length", Integer.toString(payloadBytes.length));
         response.setHeader(TextReplayRenderer.GUESSED_CHARSET_HEADER, "UTF-8");
         response.setHeader("Content-Type", "text/html");
+        response.setHeader("X-UA-Compatible", "IE=edge");
         response.setHeader(EasyMock.matches("X-Archive-Orig-.*"), EasyMock.<String>notNull());
         EasyMock.expectLastCall().anyTimes();
-        
+
         EasyMock.replay(nodeHandler, response, uriConverter);
-        
+
         cut.renderResource(null, response, wbRequest, result, payloadResource, payloadResource, uriConverter, null);
-        
+
         EasyMock.verify(nodeHandler, response, uriConverter);
-        
+
         // NOTE: this compares output of Node.toHtml() with the original input.
         // there's a good chance of Node.toHtml() producing different text than original HTML.
         String out = servletOutput.getString();
         assertEquals("servlet output", payload, out);
-        
+
         ReplayParseContext context = parseContextCapture.getValue();
         // testing indirectly because ReplayParseContext has no method returning baseUrl.
         assertEquals("baseUrl is correctly set up", "http://www.example.com/a.html", context.resolve("a.html"));
     }
-    
+
     /**
      * test revisit record (in new format with HTTP headers).
      * @throws Exception
@@ -193,28 +194,29 @@ public class ArchivalUrlSAXRewriteReplayRendererTest extends TestCase {
         response.setHeader("Content-Length", Integer.toString(payloadBytes.length));
         response.setHeader(TextReplayRenderer.GUESSED_CHARSET_HEADER, "UTF-8");
         response.setHeader("Content-Type", "text/html");
+        response.setHeader("X-UA-Compatible", "IE=edge");
         response.setHeader(EasyMock.matches("X-Archive-Orig-.*"), EasyMock.<String>notNull());
         EasyMock.expectLastCall().anyTimes();
-        
+
         EasyMock.replay(nodeHandler, response, uriConverter);
-        
+
         cut.renderResource(null, response, wbRequest, result, headerResource, payloadResource, uriConverter, null);
-        
+
         EasyMock.verify(nodeHandler, response, uriConverter);
-        
+
         // NOTE: this compares output of Node.toHtml() with the original input.
         // there's a good chance of Node.toHtml() producing different text than original HTML.
         String out = servletOutput.getString();
         assertEquals("servlet output", payload, out);
-        
+
         ReplayParseContext context = parseContextCapture.getValue();
         // testing indirectly because ReplayParseContext has no method returning baseUrl.
         assertEquals("baseUrl is correctly set up", "http://www.example.com/a.html", context.resolve("a.html"));
     }
-    
+
     // no test for old-style revisit record as headerResource, because it is caller's responsibility to
     // set headerResource = payloadResource in this case.
-    
+
 //    /**
 //     * test revisit record (in old format without HTTP headers).
 //     * @throws Exception
@@ -240,50 +242,50 @@ public class ArchivalUrlSAXRewriteReplayRendererTest extends TestCase {
 //        response.setHeader("Content-Type", "text/html");
 //        response.setHeader(EasyMock.matches("X-Archive-Orig-.*"), EasyMock.<String>notNull());
 //        EasyMock.expectLastCall().anyTimes();
-//        
+//
 //        EasyMock.replay(nodeHandler, response, uriConverter);
-//        
+//
 //        cut.renderResource(null, response, wbRequest, result, headerResource, payloadResource, uriConverter, null);
-//        
+//
 //        EasyMock.verify(nodeHandler, response, uriConverter);
-//        
+//
 //        // NOTE: this compares output of Node.toHtml() with the original input.
 //        // there's a good chance of Node.toHtml() producing different text than original HTML.
 //        String out = servletOutput.getString();
 //        assertEquals("servlet output", payload, out);
-//        
+//
 //        ReplayParseContext context = parseContextCapture.getValue();
 //        // testing indirectly because ReplayParseContext has no method returning baseUrl.
 //        assertEquals("baseUrl is correctly set up", "http://www.example.com/a.html", context.resolve("a.html"));
 //    }
 
     public void testDoneFlagSetForFrameset() throws Exception {
-        String payload = "<frameset cols=\"25%,*,25%\">\n" + 
+        String payload = "<frameset cols=\"25%,*,25%\">\n" +
                 "  <frame src=\"top.html\">\n" +
                 "  <frame src=\"center.html\">\n" +
                 "  <frame src=\"bottom.html\">\n" +
                 "</frameset>\n";
         byte[] payloadBytes = payload.getBytes("UTF-8");
         Resource payloadResource = createTestHtmlResource(payloadBytes);
-        
+
         Capture<ReplayParseContext> parseContextCapture = new Capture<ReplayParseContext>();
         nodeHandler.handleParseStart(EasyMock.capture(parseContextCapture));
         nodeHandler.handleParseComplete(EasyMock.<ReplayParseContext>anyObject());
         nodeHandler.handleNode(EasyMock.<ParseContext>anyObject(), EasyMock.<Node>anyObject());
         EasyMock.expectLastCall().anyTimes();
-        
+
         // do not care about these expectations in this test.
         response.setStatus(200);
         response.setCharacterEncoding("utf-8");
         response.setHeader(EasyMock.<String>notNull(), EasyMock.<String>notNull());
         EasyMock.expectLastCall().anyTimes();
-        
+
         EasyMock.replay(nodeHandler, response, uriConverter);
 
         cut.renderResource(null, response, wbRequest, result, payloadResource, payloadResource, uriConverter, null);
-        
+
         EasyMock.verify(nodeHandler, response, uriConverter);
-        
+
         ReplayParseContext context = parseContextCapture.getValue();
         assertNotNull(context);
         // it's a kind of odd to use this constant defined in FastArchivalUrlReplayParseEventHandler.
@@ -292,37 +294,37 @@ public class ArchivalUrlSAXRewriteReplayRendererTest extends TestCase {
         assertNotNull("FERRET_DONE flag is set",
                 context.getData(FastArchivalUrlReplayParseEventHandler.FERRET_DONE_KEY));
     }
-    
+
     public void testDoneFlagNotSetForFrameWrapperContext() throws Exception {
-        String payload = "<frameset cols=\"25%,*,25%\">\n" + 
+        String payload = "<frameset cols=\"25%,*,25%\">\n" +
                 "  <frame src=\"top.html\">\n" +
                 "  <frame src=\"center.html\">\n" +
                 "  <frame src=\"bottom.html\">\n" +
                 "</frameset>\n";
         byte[] payloadBytes = payload.getBytes("UTF-8");
         Resource payloadResource = createTestHtmlResource(payloadBytes);
-        
+
         Capture<ReplayParseContext> parseContextCapture = new Capture<ReplayParseContext>();
         nodeHandler.handleParseStart(EasyMock.capture(parseContextCapture));
         nodeHandler.handleParseComplete(EasyMock.<ReplayParseContext>anyObject());
         nodeHandler.handleNode(EasyMock.<ParseContext>anyObject(), EasyMock.<Node>anyObject());
         EasyMock.expectLastCall().anyTimes();
-        
+
         // do not care about these expectations in this test.
         response.setStatus(200);
         response.setCharacterEncoding("utf-8");
         response.setHeader(EasyMock.<String>notNull(), EasyMock.<String>notNull());
         EasyMock.expectLastCall().anyTimes();
-        
+
         EasyMock.replay(nodeHandler, response, uriConverter);
 
         // !!! KEY SETTING OF THIS TEST !!!
         wbRequest.setFrameWrapperContext(true);
-        
+
         cut.renderResource(null, response, wbRequest, result, payloadResource, payloadResource, uriConverter, null);
-        
+
         EasyMock.verify(nodeHandler, response, uriConverter);
-        
+
         ReplayParseContext context = parseContextCapture.getValue();
         assertNotNull(context);
         // it's kind of odd to use this constant defined in FastArchivalUrlReplayParseEventHandler.
