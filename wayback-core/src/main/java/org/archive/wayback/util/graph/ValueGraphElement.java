2c583a6f54032762d4dd56207a9fec842dacfd6b
Naomi Dushay <ndushay@stanford.edu>
2017-08-07 19:04:16 -0700

    core: util/graph/ValueGraphElement.java - draw() fillRect called with width-1

--- wayback-core/src/main/java/org/archive/wayback/util/graph/ValueGraphElement.java
+++ wayback-core/src/main/java/org/archive/wayback/util/graph/ValueGraphElement.java
@@ -47,6 +47,7 @@ public class ValueGraphElement extends RectangularGraphElement {
 
   public void draw(Graphics2D g2d) {
     g2d.setColor(highlighted ? config.valueHighlightColor : config.valueColor);
-    g2d.fillRect(x, y, width, height);
+    // g2d.fillRect(x, y, width, height);
+    g2d.fillRect(x, y, width-1, height); // Stanford
   }
 }
